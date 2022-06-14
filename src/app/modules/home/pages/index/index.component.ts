110f47b758c5afcdd158849e33a8283d808ecd19
KingIdoApp <43930548+KingIdoApp@users.noreply.github.com>
2022-06-14 23:41:24 +0700

    update

--- src/app/modules/home/pages/index/index.component.ts
+++ src/app/modules/home/pages/index/index.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-index',
@@ -8,11 +10,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class IndexComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, public http: HttpClient) {
 
   }
 
   ngOnInit(): void {
+    this.http.get(`${environment.apiUrl}/flight`).subscribe(res=>{
+      console.log(res);
+
+    })
     console.log(this.route.snapshot.data);
   }
 
